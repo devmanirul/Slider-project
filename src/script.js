accf8704bb52f07c81cd1b2c315ad2f8a62cd0a2
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-04-29 23:20:26 +0600

    first commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.waltonphone').slick({
+    //   setting-name: setting-value
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    });
+  });
+      
